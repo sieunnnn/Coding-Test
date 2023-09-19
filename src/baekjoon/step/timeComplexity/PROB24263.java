c130cbcfaa608ea382533c88e53f70706d6ef13d
sieunnnn <wldsmtldsm65@gmail.com>
2023-09-19 17:58:09 +0900

    백준/단계별: 알고리즘 수업 - 알고리즘의 수행 시간 2 문제풀이 (#17)

---
+++ src/baekjoon/step/timeComplexity/PROB24263.java
@@ -0,0 +1,16 @@
+package baekjoon.step.timeComplexity;
+
+import java.util.Scanner;
+
+public class PROB24263 {
+    // for 문이 존재한다. 이 연산은 O(n) 시간 복잡도를 가진다.
+
+    static Scanner sc = new Scanner(System.in);
+    static int n;
+
+    public static void main(String[] args) {
+        n = sc.nextInt();
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
