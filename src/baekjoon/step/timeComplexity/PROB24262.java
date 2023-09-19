a0cc9e7da62c5d91f08c7e66003ffa7547a553a4
sieunnnn <wldsmtldsm65@gmail.com>
2023-09-19 17:55:14 +0900

    백준/단계별: 알고리즘 수업 - 알고리즘의 수행 시간 1 문제풀이 (#17)

---
+++ src/baekjoon/step/timeComplexity/PROB24262.java
@@ -0,0 +1,9 @@
+package baekjoon.step.timeComplexity;
+
+public class PROB24262 {
+    // i = ⌊n / 2⌋; 이 연산은 O(1) 시간 복잡도를 가진다.
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
