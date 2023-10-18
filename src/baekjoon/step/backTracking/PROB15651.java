8dad9848e635fa75aa0c3a7365e2af5bae659511
sieunnnn <wldsmtldsm65@gmail.com>
2023-10-18 22:43:07 +0900

    백준/단계별: 15651 N 과 M (3) 문제풀이 (#26)

---
+++ src/baekjoon/step/backTracking/PROB15651.java
@@ -0,0 +1,39 @@
+package baekjoon.step.backTracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class PROB15651 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int[] numbers;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        numbers = new int[M];
+        backTracking(0);
+        System.out.println(sb);
+    }
+
+    public static void backTracking(int depth) {
+        if (depth == M) {
+            for (int number : numbers) {
+                sb.append(number).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i ++) {
+            numbers[depth] = i + 1;
+            backTracking(depth + 1);
+        }
+    }
+}
