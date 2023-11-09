9605f7393a03930176b79b542c0cda7d47c0be1d
sieunnnn <wldsmtldsm65@gmail.com>
2023-11-09 16:21:30 +0900

    백준/단계별: 1654 랜선자르기 (#30)

---
+++ src/baekjoon/step/binarySearch/PROB1654.java
@@ -0,0 +1,53 @@
+package baekjoon.step.binarySearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class PROB1654 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int K, N;
+    static long[] lengths;
+    static int count;
+    static long max = 0;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        lengths = new long[K];
+
+        for (int i = 0; i < K; i ++) {
+            lengths[i] = Long.parseLong(br.readLine());
+
+            if (i > 0) {
+                max = Math.max(max, lengths[i]);
+            }
+        }
+        solve();
+    }
+
+    public static void solve() {
+        long minLength = 1;
+        long maxLength = max;
+        long half;
+
+        while(minLength <= maxLength) {
+            count = 0;
+            half = (minLength + maxLength) / 2;
+
+             for (int i = 0; i < K; i ++) {
+                 count += lengths[i] / half;
+             }
+
+             if (count < N) {
+                 maxLength = half - 1;
+             } else {
+                 minLength = half + 1;
+             }
+        }
+        System.out.println(maxLength);
+    }
+}
