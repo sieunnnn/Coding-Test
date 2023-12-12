cc7a9180a62139fe49d20fda8ead1e6e9e2bc694
sieunnnn <wldsmtldsm65@gmail.com>
2023-12-12 17:38:23 +0900

    알고리즘/Hash: 완주하지 못한 선수 (#39)

---
+++ src/programmers/algorithmKit/hash/PROB01.java
@@ -0,0 +1,30 @@
+package programmers.algorithmKit.hash;
+import java.util.*;
+
+public class PROB01 {
+    static Map<String, Integer> map = new HashMap<>();
+    static String answer = "";
+
+    public String solution(String[] participants, String[] completions) {
+
+        for (String participant : participants) {
+            map.put(participant, map.getOrDefault(participant, 0) + 1);
+        }
+
+        for (String completion : completions) {
+            map.put(completion, map.get(completion) - 1);
+            if (map.get(completion) == 0) {
+                map.remove(completion);
+            }
+        }
+
+        for (String participant : participants) {
+            if (map.containsKey(participant)) {
+                answer = participant;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
