f6d63d4c1b3b4252b1ae3a9e495c7cfbb4b3c139
sieunnnn <wldsmtldsm65@gmail.com>
2023-12-12 17:53:54 +0900

    알고리즘/Hash: 포켓몬 (#39)

---
+++ src/programmers/algorithmKit/hash/PROB02.java
@@ -0,0 +1,25 @@
+package programmers.algorithmKit.hash;
+import java.util.*;
+
+public class PROB02 {
+    static Set<Integer> numbers = new HashSet<>();
+
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        for (int num : nums) {
+            numbers.add(num);
+        }
+
+        int numbersSize = numbers.size();
+        int selectValue = nums.length / 2;
+
+        if (selectValue >= numbersSize) {
+            answer = numbersSize;
+        } else {
+            answer = selectValue;
+        }
+
+        return answer;
+    }
+}
