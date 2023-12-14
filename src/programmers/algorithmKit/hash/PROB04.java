5c9bbc79beafb595758c16e0b8f93a3389e70dbd
sieunnnn <wldsmtldsm65@gmail.com>
2023-12-15 00:53:04 +0900

    프로그래머스/Hash: 의상 (#41)

---
+++ src/programmers/algorithmKit/hash/PROB04.java
@@ -0,0 +1,25 @@
+package programmers.algorithmKit.hash;
+
+import java.util.*;
+
+class Solution {
+    static Map<String, Integer> clothTypes = new HashMap<>();
+
+    public int solution(String[][] clothes) {
+        int answer = 1;
+
+        for(String[] cloth : clothes) {
+            if (clothTypes.containsKey(cloth[1])) {
+                clothTypes.put(cloth[1], clothTypes.get(cloth[1]) + 1);
+            } else {
+                clothTypes.put(cloth[1], 1);
+            }
+        }
+
+        for (int value : clothTypes.values()) {
+            answer *= (value + 1);
+        }
+
+        return answer - 1;
+    }
+}
