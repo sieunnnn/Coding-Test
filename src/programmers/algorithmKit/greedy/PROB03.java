dac2e0e67d4e1336f24c2cd9d809289350e41aaa
sieunnnn <wldsmtldsm65@gmail.com>
2024-01-10 16:55:46 +0900

    프로그래머스/Greedy: 구명보트 (#48)

---
+++ src/programmers/algorithmKit/greedy/PROB03.java
@@ -0,0 +1,21 @@
+package programmers.algorithmKit.greedy;
+
+import java.util.*;
+
+public class PROB03 {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0, j = people.length - 1;
+        int boats = 0;
+
+        while (i <= j) {
+            if (people[i] + people[j] <= limit) {
+                i++;
+            }
+            j--;
+            boats++;
+        }
+
+        return boats;
+    }
+}
